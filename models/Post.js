372ad48bbdf9291237af78898bec617cb308c813
Ngalla Juke <47453602+NgallaJuke@users.noreply.github.com>
2020-04-02 23:41:01 +0000

    Initilize Tailor porject

---
+++ models/Post.js
@@ -0,0 +1,8 @@
+const Post = {
+  photo, // max 4
+  description,
+  tag,
+  createdAt,
+  postedBy, // from User
+  comments //by User and the comment
+};
---
+++ models/User.js
@@ -0,0 +1,10 @@
+const User = {
+  email,
+  first_name,
+  last_name,
+  user_name,
+  password,
+  role, // tailor ou visitor
+  createdAt,
+  following
+};
