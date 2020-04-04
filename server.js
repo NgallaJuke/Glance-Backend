def10ba463b11923067da6d9d4146273c803e8a0
Ngalla Juke <47453602+NgallaJuke@users.noreply.github.com>
2020-04-04 19:11:33 +0000

    Connection to Mongose Atlas

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const color = require("colors");
 const app = express();
 
-app.listen(3000, () => console.log("app runing on port 3000"));
+//require env variables
+dotenv.config({ path: "./config/config.env" });
+
+//database connection
+const DBconnect = require("./config/db.js");
+DBconnect();
+
+// setting up port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`App running on port ${PORT}`.blue.bold));
