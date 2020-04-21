4b127007497eb310648428e8bcd34a7fe6c1ee79
Ngalla Juke <47453602+NgallaJuke@users.noreply.github.com>
2020-04-21 00:27:09 +0000

    Using Gmail to send mail ! chang the values in the config file

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,8 +3,10 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
+    service: "gmail",
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
