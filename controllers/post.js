005f135e30762c55a48abfdc3bf4b4b235dc478f
Ngalla Juke <47453602+NgallaJuke@users.noreply.github.com>
2020-04-30 02:59:57 +0000

    Fixing bug about the amount of photos a post can have

--- controllers/post.js
+++ controllers/post.js
@@ -16,8 +16,9 @@ exports.CreatePost = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse("Please add a photo", 400));
   }
 
-  // save the files image
-  Array.from(req.files.img_url).forEach((file) => {
+  console.log("req.files", Array.from(req.files.img_url).length);
+  if (Array.from(req.files.img_url).length === 0) {
+    const file = req.files.img_url;
     // make sure the file is an image
     if (!file.mimetype.startsWith("image")) {
       error = new ErrorResponse("Please upload an image file", 403);
@@ -37,16 +38,10 @@ exports.CreatePost = asyncHandler(async (req, res, next) => {
     }
 
     // Create costum file name
-    file.name = `post_img[${count}]_${Date.now()}${path.parse(file.name).ext}`;
-    count++;
+    file.name = `post_img[0]_${Date.now()}${path.parse(file.name).ext}`;
+
     img_url.push(file.name);
     // move the file
-    files.push(file);
-  });
-  if (error) return console.log("Error :", error);
-
-  //move all the files to publis folder
-  files.forEach((file) => {
     file.mv(`${process.env.POSTS_PIC_PATH}/${file.name}`, async (err) => {
       if (err) {
         return next(
@@ -57,16 +52,62 @@ exports.CreatePost = asyncHandler(async (req, res, next) => {
         );
       }
     });
-  });
+  } else {
+    // save the files image
+    Array.from(req.files.img_url).forEach((file) => {
+      // make sure the file is an image
+      if (!file.mimetype.startsWith("image")) {
+        error = new ErrorResponse("Please upload an image file", 403);
+        return next(error);
+      } // make sure the image is not a gif
+      if (file.mimetype === "image/gif") {
+        error = new ErrorResponse("Gif image is not allow", 403);
+        return next(error);
+      }
+      // check file size
+      if (file.size > process.env.MAX_PIC_SIZE) {
+        error = new ErrorResponse(
+          `Please upload an image less than ${process.env.MAX_PIC_SIZE}Mb`,
+          400
+        );
+        return next(error);
+      }
+
+      // Create costum file name
+      file.name = `post_img[${count}]_${Date.now()}${
+        path.parse(file.name).ext
+      }`;
+      count++;
+      img_url.push(file.name);
+      // move the file
+      files.push(file);
+    });
+    if (error) return console.log("Error :", error);
+
+    //move all the files to publis folder
+    files.forEach((file) => {
+      file.mv(`${process.env.POSTS_PIC_PATH}/${file.name}`, async (err) => {
+        if (err) {
+          return next(
+            new ErrorResponse(
+              `Problem while uploading the file ${file.name}`,
+              500
+            )
+          );
+        }
+      });
+    });
+  }
 
   // check if the description has any #(tags) in it
-
   const reg = /#\S+/g;
   let tags = [];
   if (req.body.description.match(reg)) {
     tags = req.body.description.match(reg);
   }
 
+  if (img_url.length === 0)
+    return next(new ErrorResponse("Error while uploading the photos", 500));
   const post = await Post.create({
     img_url,
     description: req.body.description,
